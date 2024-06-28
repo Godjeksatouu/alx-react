e647fd315ebf8929dd3f08b1761415820c806222
Godjeksatouu <godjeksatou@gmail.com>
2024-06-28 10:03:13 -0700

    task_0

--- 0x00-webpack/task_0/src/index.js
+++ 0x00-webpack/task_0/src/index.js
@@ -1,6 +1,5 @@
-mport $ from 'jquery';
+const $ = require( "jquery" );
 
-// add three different p tags to body
-// $('body').append('<p>Holberton Dashboard</p>');
-// $('body').append('<p>Dashboard data for the students</p>');
-// $('body').append('<p>Copyright - Holberton School</p>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
