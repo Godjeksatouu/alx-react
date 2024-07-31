8e3ef800fdf1c21ebba3032bd83f81972a5646bc
Godjeksatouu <godjeksatou@gmail.com>
2024-07-31 23:02:20 +0100

    redux

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/__mocks__/styleMock.js
@@ -0,0 +1,25 @@
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+styleMock.js [unix] (00:59 01/01/1970)                                 0,0-1 All
+"styleMock.js" [New]
+module.exports = {};
